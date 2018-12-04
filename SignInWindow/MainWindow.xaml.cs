93bde25136f20f2031ff82976e58a4065ff162ab
Anton <example@gmail.com>
2018-12-04 21:12:33 +0200

    Create MainWindow and SignInWindow

---
+++ SignInWindow/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SignInWindow
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void checkInButt_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                CheckInWindow check = new CheckInWindow();
+                check.Show();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void signInButt_Click(object sender, RoutedEventArgs e)
+        {
+            if (userNameTextBox.Text.Length > 0)
+            {
+                MessageBox.Show("ok");
+            }
+            else
+            {
+                MessageBox.Show("Enter login");
+            }
+        }
+    }
+}
